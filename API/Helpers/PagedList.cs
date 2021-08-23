7d77799f88e0403fd1e53fa972870ee9d3470d28
Mongkol Chocktaweechock <mongkol.gk@gmail.com>
2021-08-23 21:36:50 +0700

    End of section 15

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Helpers
@@ -28,6 +29,11 @@ namespace API.Helpers
             var count = await source.CountAsync();
             var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+        internal static Task<PagedList<MessageDto>> CreateAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
